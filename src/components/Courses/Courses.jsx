8152264d521d5096934a0c2b44f2d43d890d28e8
Yevhenii Khlopin <ykhlopin@assurance.com>
2023-02-21 00:19:01 +0200

    Add search functionality

--- src/components/Courses/Courses.jsx
+++ src/components/Courses/Courses.jsx
@@ -1,21 +1,35 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import CourseCard from './components/CourseCard/CourseCard';
 import Button from 'common/Button/Button';
 import SearchBar from './components/SearchBar/SearchBar';
 
 const Courses = (props) => {
+	const [searchValue, setSearchValue] = useState('');
+
+	const searchSubmitHandler = (value) => setSearchValue(value);
+
+	// TODO: prevent searchCourse from being called twice
+	const searchCourse = function () {
+		if (searchValue === '') return props.coursesList;
+		return props.coursesList.filter((course) =>
+			course.title.toLowerCase().includes(searchValue.toLowerCase())
+		);
+	};
+
+	useEffect(() => {}, [searchValue]);
+
 	return (
 		<>
 			<nav className='navbar mb-2'>
 				<div className='container-fluid'>
-					<SearchBar></SearchBar>
+					<SearchBar onSearch={searchSubmitHandler}></SearchBar>
 					<div className='d-flex'>
 						<Button buttonText='Add new course'></Button>
 					</div>
 				</div>
 			</nav>
 			<div className='container-fluid'>
-				{props.coursesList.map((course, index) => (
+				{searchCourse().map((course, index) => (
 					<div className='mb-2' key={index}>
 						<CourseCard
 							courseData={course}
--- src/components/Courses/components/SearchBar/SearchBar.jsx
+++ src/components/Courses/components/SearchBar/SearchBar.jsx
@@ -1,14 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Button from 'common/Button/Button';
 
 const SearchBar = (props) => {
+	// TODO: Think about a better state variable name for the search value
+	const [value, setValue] = useState('');
+
+	const handleChange = function (event) {
+		// Submit empty form without clicking the search button
+		if (event.target.value === '') props.onSearch('');
+		setValue(event.target.value);
+	};
+
+	const handleSubmit = function (event) {
+		event.preventDefault();
+		props.onSearch(value);
+	};
+
 	return (
-		<form className='d-flex' role='search'>
+		<form className='d-flex' onSubmit={handleSubmit}>
 			<input
 				className='form-control me-2'
 				type='search'
 				placeholder='Search'
 				aria-label='Search'
+				onChange={handleChange}
 			></input>
 			<Button className='btn btn-outline-success' buttonText='Search'></Button>
 		</form>
